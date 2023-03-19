fd0d6ea208102e955ef903b43806bb736df68836
Dmytro2V <op2vin@gmail.com>
2023-03-20 01:14:15 +0200

    done with bonus

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 // Import package
 
 // Your code here
+const jwt = require('jsonwebtoken');
 
 // Define variables - DO NOT MODIFY
 let token;
@@ -12,6 +13,8 @@ let payload;
 // 1. Sign (create) a JWT containing your email address
 
 // Your code here
+payload = {email:'my@email.com'};
+token = jwt.sign(payload, process.env.SECRET_KEY, {expiresIn: '1h'});
 
 // See the JWT in the console
 console.log('JWT:', token);
@@ -19,6 +22,7 @@ console.log('JWT:', token);
 // 2. Decode a JWT Payload
 
 // Your code here
+payload = jwt.decode(token)
 
 // See the decoded payload in the console
 console.log('Payload:', payload);
@@ -26,9 +30,10 @@ console.log('Payload:', payload);
 // 3. Verify a JWT
 
 // Your code here
+payload = jwt.verify(token, process.env.SECRET_KEY)
 
 // See the verified payload in the console
-console.log('Verified Payload:', payload);
+console.log('Verified Payload:', payload, {expiresIn: '5m'});
 
 // (Optional) Bonus: Catch Error With Invalid Signature
 // Generate an alternate secret key and use it
@@ -36,7 +41,17 @@ console.log('Verified Payload:', payload);
 //    Then "catch" the error and log it to the console.
 
 // Your code here
+let token2 = jwt.sign(payload, 'second-key');
 
+console.log('JWT:', token);
+try{
+    payload = jwt.verify(token2, 'not-key')
+    console.log('still ok - bad');
+} catch(err) {
+    console.log('catched');
+    console.log(err.message);
+}
+console.log('?????????????');
 // (Optional) Bonus: Catch Error With Expired Token
 // First, set the token's expiration (above) to 1 second
 // Second, add a setTimeout longer than 1 second
@@ -44,3 +59,16 @@ console.log('Verified Payload:', payload);
 //    Then "catch" the error and log it to the console
 
 // Your code here
+payload = {next:'next'} // need be an object to use options
+let token3 = jwt.sign(payload, 'timed-key', {expiresIn: '1s'});
+setTimeout(() => {
+    console.log('JWT:-----delayed', token);
+    try{
+        payload = jwt.verify(token3, 'timed-key')
+        console.log('still ok');
+    } catch(err) {
+        console.log('expired');
+        console.log(err.message);
+    }
+}, 5000);
+
